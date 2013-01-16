742ddf7574fc569317a50e0b5199a48db459cf3a
denispeplin <denis.peplin@gmail.com>
2013-01-16 14:34:29 +0400

    New Rails 3.2.11 application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExtjsTut::Application.config.secret_token = 'da5e90d146ba62fe33564768a66178ae3c4274e732e2206e134b8557eb10ce30d9b60c35f14bf96cbb59e6a5f56e1dd8ee1104c6900e8bc7f65db35ba5412648'
