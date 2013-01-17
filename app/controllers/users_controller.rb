1e2ed7223d55460b89b1fa6e64ddf99bec15ff7a
denispeplin <denis.peplin@gmail.com>
2013-01-17 15:56:21 +0400

    Add 'update' action to Users contoller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,16 @@ class UsersController < ApplicationController
       format.json { render json: @users }
     end
   end
+
+  def update
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.json { head :no_content }
+      else
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
