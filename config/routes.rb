56ed1a8069fa47875999f88165de300f720de6b6
denispeplin <denis.peplin@gmail.com>
2013-01-22 09:17:21 +0400

    Root to netzke's pages#index

--- config/routes.rb
+++ config/routes.rb
@@ -55,7 +55,7 @@ ExtjsTut::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'hello#index'
+  root :to => 'pages#index'
 
   # See how all your routes lay out with "rake routes"
 
